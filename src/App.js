e7df3a2d4cf8ffacdb0c6a602066ffccffd81f71
AshleyThomasDevelopment <ashleythomasdevelopment@gmail.com>
2023-12-03 22:47:57 -0800

    Initial commit with cube

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// import Tester from './tester.component'
+
+import Scene from "./components/scene.component";
+
+export function App() {
+    return (
+        <>
+            <h1>Hello world!</h1>
+            <Scene />
+            {/* <Tester /> */}
+        </>
+    )
+}
---
+++ src/components/scene.component.js
@@ -0,0 +1,56 @@
+
+import { useEffect, useRef } from 'react';
+import * as THREE from 'three';
+
+import { OrbitControls } from '../../node_modules/three/examples/jsm/controls/OrbitControls'
+
+
+const Scene = () => {
+    // const loader = new GLTFLoader();
+
+    const sceneRef = useRef(null)
+
+    useEffect(() => {
+
+        const scene = new THREE.Scene();
+        const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+        const renderer = new THREE.WebGLRenderer();
+        const controls = new OrbitControls(camera, renderer.domElement);
+        renderer.setSize(window.innerWidth, window.innerHeight);
+
+        sceneRef.current.appendChild(renderer.domElement);
+
+
+        camera.position.z = 5;
+
+        const light = new THREE.AmbientLight(0x404040, 0.5); // soft white light
+
+        const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+        directionalLight.position.set(5, 5, 5).normalize();
+
+        const hemisphereLight = new THREE.HemisphereLight(0xffffff, 0x000000, 0.6);
+        
+        const geometry = new THREE.BoxGeometry(1, 1, 1);
+        const material = new THREE.MeshNormalMaterial({ color: 0x00ff00 });
+        const depth = new THREE.MeshDepthMaterial({ color: 0x40ff10 });
+
+        const cube = new THREE.Mesh(geometry, material, depth);
+        scene.add(directionalLight, cube, hemisphereLight, light );
+
+        function animate() {
+            requestAnimationFrame(animate);
+            cube.rotation.x += 0.01;
+            cube.rotation.y += 0.01;
+            renderer.render(scene, camera);
+        }
+        animate();
+    }, [])
+
+    return (
+        <div ref={sceneRef} />
+    )
+
+}
+
+export default Scene
